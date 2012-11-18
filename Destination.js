546dd0d11944f1ccfa366c1710641793b901c1fe
Sara Hemmings <hemmings.sara@gmail.com>
2012-11-18 15:16:21 -0500

    Oh no where did the maps go :(

--- Destination.js
+++ Destination.js
@@ -1,3 +1,7 @@
+var driveTime; //global variable for the driving time, for calculations later
+var transitTime; //global variable for the transit time, for calculations
+
+
 function loadDriveDirections() {
 	var directionsReq = {
 			origin: document.getElementById("startAddress").value,
@@ -18,11 +22,17 @@ function loadDriveDirections() {
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 			};
 			
-		console.log(results);
+		console.log(results); //debugging
+		
 		var map = new google.maps.Map(document.getElementById("map_canvas1"), mapOptions);
 		directionsDisplay.setMap(map);
 			
 		directionsDisplay.setDirections(results);
+		
+		driveTime = results.routes[0].legs[0].duration.value;
+		console.log(driveTime); //debugging
+		
+		timeCalc();
 	});
 }
 
@@ -51,11 +61,44 @@ function loadTransitDirections() {
 		directionsDisplay.setMap(map);
 			
 		directionsDisplay.setDirections(results);
+		
+		transitTime = results.routes[0].legs[0].duration.value;
+		
+		timeCalc();
+		
+		console.log(transitTime); //debugging
 	});
+}
+	
+function timeCalc() { //tells the user which route is faster
+	var timeDifference; //the time difference between the two routes
+	
+	if (driveTime) {
+		document.getElementById("timeDiv1").innerHTML = "Driving Route: " + Math.round(driveTime / 60) + " minutes";
+	}
+	
+	if (transitTime) {
+		document.getElementById("timeDiv2").innerHTML = "Transit Route: " + Math.round(transitTime / 60) + " minutes";
+	}
+	
+	if(driveTime && transitTime) {
+		if (driveTime < transitTime) {
+			timeDifference = transitTime - driveTime;
+			timeDifference = Math.round(timeDifference / 60); //TODO: figure out how to round value.
+			document.getElementById("resultsDiv").innerHTML = "Driving to this destination is faster by " + timeDifference + " minutes.";
+		} else if (transitTime < driveTime) {
+			timeDifference = driveTime - transitTime;
+			timeDifference = Math.round(timeDifference / 60); //TODO: figure out how to round value.
+			document.getElementById("resultsDiv").innterHTML = "Taking public transit to this destination is faster by " + timeDiffence + " minutes";
+		} else {
+			document.getElementById("resultsDiv").innerHTML = "Both routes take the same amount of time.";
+		}
+	console.log('fuuuuuu', timeDifference);
+	}	
 }
 
 function onSubmitClick() {
-    alert("CONGRATULATIONS! YOU CLICKED A BUTTON");
+    //alert("CONGRATULATIONS! YOU CLICKED A BUTTON");
     loadDriveDirections();
     loadTransitDirections();
 }
