9ee4fb514714c7228fc0476ce99766d8b70f4e81
Sara Hemmings <hemmings.sara@gmail.com>
2012-11-18 13:33:42 -0500

    Transit map added

--- Destination.js
+++ Destination.js
@@ -1,4 +1,4 @@
-function loadDirections() {
+function loadDriveDirections() {
 	var directionsReq = {
 			origin: document.getElementById("startAddress").value,
 			destination: document.getElementById("destination").value,
@@ -19,7 +19,35 @@ function loadDirections() {
 			};
 			
 		console.log(results);
-		var map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
+		var map = new google.maps.Map(document.getElementById("map_canvas1"), mapOptions);
+		directionsDisplay.setMap(map);
+			
+		directionsDisplay.setDirections(results);
+	});
+}
+
+function loadTransitDirections() {
+	var directionsReq = {
+			origin: document.getElementById("startAddress").value,
+			destination: document.getElementById("destination").value,
+			travelMode: google.maps.TravelMode.TRANSIT,
+			unitSystem: google.maps.UnitSystem.IMPERIAL,
+			provideRouteAlternatives: true,
+			avoidTolls: true
+		}
+		
+	var d = new google.maps.DirectionsService();
+	var directionsDisplay = new google.maps.DirectionsRenderer();
+		
+	d.route(directionsReq, function (results) {
+		var mapOptions = {
+			center: new google.maps.LatLng(38.895111, -77.036667),
+			zoom: 17,
+			mapTypeId: google.maps.MapTypeId.ROADMAP
+			};
+			
+		console.log(results);
+		var map = new google.maps.Map(document.getElementById("map_canvas2"), mapOptions);
 		directionsDisplay.setMap(map);
 			
 		directionsDisplay.setDirections(results);
@@ -28,7 +56,8 @@ function loadDirections() {
 
 function onSubmitClick() {
     alert("CONGRATULATIONS! YOU CLICKED A BUTTON");
-    loadDirections();
+    loadDriveDirections();
+    loadTransitDirections();
 }
 
 function init() {
