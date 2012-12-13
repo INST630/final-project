8926dba4e36230eb6432299329ea2685d7b1d6d7
Sara Hemmings <hemmings.sara@gmail.com>
2012-12-12 19:08:02 -0500

    Tab functionality added

--- Destination.js
+++ Destination.js
@@ -7,27 +7,27 @@ var costGas;//avg of gas costs by geolocation
 var startAddress;//global for start address
 
 function gasCallBack(gasFeedResults) {
-					var numStations=0;
-					var totalPrice=0.0;
-					/*var keys = Object.keys(gasFeedResults.stations[0]);
-					for(var i=0;i<keys.length;i++){
-						alert(keys[i]);
-					}*/
-					for (var i=0; i< gasFeedResults.stations.length; i++){
-//						alert(gasFeedResults.stations[i].reg_price);
-						var price = parseFloat(gasFeedResults.stations[i].price);
-						if ( ! isNaN(price)) {
-							totalPrice+= price;
-							numStations++;
-						}
-					}
-					
-					if (numStations===0){
-						alert ("The avg price in your region could not be found");
-					} else {
-						var avgPrice= totalPrice/numStations;
-						alert(avgPrice);
-					}
+	var numStations=0;
+	var totalPrice=0.0;
+	/*var keys = Object.keys(gasFeedResults.stations[0]);
+	for(var i=0;i<keys.length;i++){
+		alert(keys[i]);
+	}*/
+	for (var i=0; i< gasFeedResults.stations.length; i++){
+	//alert(gasFeedResults.stations[i].reg_price);
+		var price = parseFloat(gasFeedResults.stations[i].price);
+		if ( ! isNaN(price)) {
+			totalPrice+= price;
+			numStations++;
+		}
+	}
+	
+	if (numStations===0){
+		alert ("The avg price in your region could not be found");
+	} else {
+		var avgPrice= totalPrice/numStations;
+		alert(avgPrice);
+	}
 
 }
 
@@ -36,7 +36,7 @@ function loadDriveDirections() { //loads map and results for driving route: from
 			//origin: "8191 Strawberry Lane, Falls Church, VA", //set to default for debugging
 			//destination: "National Gallery of Art, Washington, DC", //set to default for debugging
 			origin: startAddress,
-			destination: document.getElementById("destination").value, //change to reflect parking 
+			destination: $("destination").value, //change to reflect parking 
 			travelMode: google.maps.TravelMode.DRIVING,
 			unitSystem: google.maps.UnitSystem.IMPERIAL,
 			provideRouteAlternatives: true,
@@ -55,15 +55,15 @@ function loadDriveDirections() { //loads map and results for driving route: from
 			
 		console.log(results); //debugging
 		
-		var map = new google.maps.Map(document.getElementById("map_canvas_car"), mapOptions); //writes the map to the DOM
+		var map = new google.maps.Map($("map_canvas_car"), mapOptions); //writes the map to the DOM
 		directionsDisplay.setMap(map);
 				
 		directionsDisplay.setDirections(results);
 			
 		driveTime = results.routes[0].legs[0].duration.value; //sets the global variable to these results for calculating drive time
 		
-		document.getElementById("carTime").innerHTML = results.routes[0].legs[0].duration.text;
-		document.getElementById("carDistance").innerHTML = results.routes[0].legs[0].distance.text;
+		$("carTime").innerHTML = results.routes[0].legs[0].duration.text;
+		$("carDistance").innerHTML = results.routes[0].legs[0].distance.text;
 		console.log(driveTime); //debugging
 			
 		var geoOptions =   {
@@ -102,7 +102,7 @@ function loadTaxiDirections() { //loads map and results for driving route: from 
 			//origin: "8191 Strawberry Lane, Falls Church, VA", //set to default for debugging
 			//destination: "National Gallery of Art, Washington, DC", //set to default for debugging
 			origin: startAddress,
-			destination: document.getElementById("destination").value,
+			destination: $("destination").value,
 			travelMode: google.maps.TravelMode.DRIVING,
 			unitSystem: google.maps.UnitSystem.IMPERIAL,
 			provideRouteAlternatives: true,
@@ -121,8 +121,8 @@ function loadTaxiDirections() { //loads map and results for driving route: from 
 			
 		console.log(results); //debugging
 		
-		var map = new google.maps.Map(document.getElementById("map_canvas_taxi"), mapOptions); //writes the map to the DOM
-		var carDirectionsDiv = document.getElementById("carDirections");
+		var map = new google.maps.Map($("map_canvas_taxi"), mapOptions); //writes the map to the DOM
+		var carDirectionsDiv = $("carDirections");
 		directionsDisplay.setMap(map);
 		directionsDisplay.setPanel(carDirectionsDiv);
 		directionsDisplay.setDirections(results);
@@ -137,10 +137,10 @@ function loadTaxiDirections() { //loads map and results for driving route: from 
 
 function loadTransitDirections() { //loads map and results for public transit route using the Google Directions API
 	var directionsReq = {
-			//origin: "8191 Strawberry Lane, Falls Church, VA", //set to default for debugging
-			//destination: "National Gallery of Art, Washington, DC", //set to default for debugging
+			origin: "8191 Strawberry Lane, Falls Church, VA", //set to default for debugging
+			destination: "National Gallery of Art, Washington, DC", //set to default for debugging
 			origin: startAddress,
-			destination: document.getElementById("destination").value,
+			destination: $("destination").value,
 			travelMode: google.maps.TravelMode.TRANSIT,
 			unitSystem: google.maps.UnitSystem.IMPERIAL,
 			provideRouteAlternatives: true
@@ -158,15 +158,13 @@ function loadTransitDirections() { //loads map and results for public transit ro
 			
 		console.log(results); //debugging
 		
-		var map = new google.maps.Map(document.getElementById("map_canvas_transit"), mapOptions);//writes the resulting map to the DOM
+		var map = new google.maps.Map($("map_canvas_transit"), mapOptions);//writes the resulting map to the DOM
 		
 		directionsDisplay.setMap(map);
 			
 		directionsDisplay.setDirections(results);
 		
-		transitTime = results.routes[0].legs[0].duration.value; //sets the global variable to the travel time for later calculations
-
-
+		transitTime = results.routes[0].legs[0].duration.value; //sets the global variable to the travel time for later calculation
 		
 //		timeCalc(); //calls timeCalc function so it doesn't try to calculate before the results are back from the API
 		
@@ -178,21 +176,21 @@ function timeCalc() { //tells the user which route is faster
 	var timeDifference; //the time difference between the three routes
 	
 	if (driveTime) { //displays the total route time for the driving route
-		document.getElementById("timeDiv1").innerHTML = "Driving Route: " + Math.round(driveTime / 60) + " minutes"; //change route to reflect parking
+		$("timeDiv1").innerHTML = "Driving Route: " + Math.round(driveTime / 60) + " minutes"; //change route to reflect parking
 		//don't grey drive
 	} else {
 		//grey drive time
 	}
 	
 	if (taxiTime){ //displays the total route time for the taxi route
-		document.getElementById("timeDiv1").innerHTML = "Driving Route: " + Math.round(taxiTime / 60) + " minutes";
+		$("timeDiv1").innerHTML = "Driving Route: " + Math.round(taxiTime / 60) + " minutes";
 		//don't grey taxi
 	}else {
 		//grey taxi
 	}
 	
 	if (transitTime) { //displays the total route time for the transit route
-		document.getElementById("timeDiv2").innerHTML = "Transit Route: " + Math.round(transitTime / 60) + " minutes";
+		$("timeDiv2").innerHTML = "Transit Route: " + Math.round(transitTime / 60) + " minutes";
 		//don't grey transit
 	}else {
 		//grey transit
@@ -200,22 +198,22 @@ function timeCalc() { //tells the user which route is faster
 	
 	//calculates the time saved when choosing between 3 routes. Tells which is shorter and by how long.
 	if(driveTime && transitTime && taxiTime ) { //makes sure both variables have a value before attempting to calculate the values, so as to avoid a return of undefined
-		if (driveTime < transitTime && driveTime< taxiTime) {
+		if (driveTime < transitTime && driveTime < taxiTime) {
 			timeDifference = transitTime - driveTime;
 			timeDifference = Math.round(timeDifference / 60); //Google API measures time in seconds, so is converted to minutes and rounded to nearest integer
-			document.getElementById("resultsDiv").innerHTML = "Driving to this destination is faster by " + timeDifference + " minutes.";
+			$("resultsDiv").innerHTML = "Driving to this destination is faster by " + timeDifference + " minutes.";
 		
-		} else if (taxiTime < transitTime &&  driveTime < driveTime) {
+		} else if (taxiTime < transitTime &&  taxtTime < driveTime) {
 			timeDifference = transitTime - taxiTime;
 			timeDifference = Math.round(timeDifference / 60); //Google API measures time in seconds, so is converted to minutes and rounded to nearest integer
-			document.getElementById("resultsDiv").innerHTML = "Taking a taxi to this destination is faster by " + timeDifference + " minutes.";	
+			$("resultsDiv").innerHTML = "Taking a taxi to this destination is faster by " + timeDifference + " minutes.";	
 			
 		} else if (transitTime < driveTime && driveTime < taxiTime) {
 			timeDifference = driveTime - transitTime;
 			timeDifference = Math.round(timeDifference / 60); //Google API measures time in seconds, so is converted to minutes and rounded to nearest integer
-			document.getElementById("resultsDiv").innterHTML = "Taking public transit to this destination is faster by " + timeDiffence + " minutes";
+			$("resultsDiv").innterHTML = "Taking public transit to this destination is faster by " + timeDiffence + " minutes";
 		} else {
-			document.getElementById("resultsDiv").innerHTML = "All routes take the same amount of time."; //if all are equal, alerts the user
+			$("resultsDiv").innerHTML = "All routes take the same amount of time."; //if all are equal, alerts the user
 		}
 		
 	} else if (driveTime && transitTime ){
@@ -234,7 +232,7 @@ function timeCalc() { //tells the user which route is faster
 function onSubmitClick() { //calls the map loading functions when the user clicks the submit button
     //alert("CONGRATULATIONS! YOU CLICKED A BUTTON");
     
-	startAddress = document.getElementById("startAddress").value;
+	startAddress = $("startAddress").value;
 	loadDriveDirections();
 	loadTransitDirections();
 	loadTaxiDirections();
@@ -242,37 +240,18 @@ function onSubmitClick() { //calls the map loading functions when the user click
     return false; //makes sure the form doesn't submit itself
 }
 
-function onCarClick(){
-	document.getElementById("carContent").style.display="block"; 
-	document.getElementById("transitContent").style.display="none";
-	document.getElementById("taxiContent").style.display="none";
-}
-
-function onTaxiClick(){
-	document.getElementById("carContent").style.display="none";
-	document.getElementById("transitContent").style.display="none";
-	document.getElementById("taxiContent").style.display="block";
-}
-
-function onTransitClick(){
-	document.getElementById("carContent").style.display="none";
-	document.getElementById("transitContent").style.display="block";
-	document.getElementById("taxiContent").style.display="none";
-}
-
 function calcCostClick(){
-	var input = document.getElementById("calcCost").checked;
+	var input = $("calcCost").checked;
 	if(input){
-	document.getElementById("hiddenCostCalculator").style.display="block";
+		$("hiddenCostCalculator").style.display="block";
 	}else{
-		
-	document.getElementById("hiddenCostCalculator").style.display="none";
+		$("hiddenCostCalculator").style.display="none";
 	}
 }
 
 function showmap(id){    
 	var geoOptions =   {
-			    address: "1633 Hydenwood Cres. Chesapeake",
+			    address: "Washington, DC",
 			    region: "us"
 			};
 		
@@ -295,18 +274,16 @@ function showmap(id){
 
 
 function init() {
-    document.getElementById("submitForm").onclick = onSubmitClick;
-    document.getElementById("informationForm").onsubmit = onSubmitClick; //after filling out form, user can submit by pressing enter
-    document.getElementById("driving").onclick=onCarClick;
-    document.getElementById("transit").onclick=onTaxiClick;
-    document.getElementById("transitLink").onclick=onTransitClick;
-    document.getElementById("calcCost").onclick=calcCostClick;
+    $("submitForm").click(onSubmitClick);
+    $("informationForm").submit(onSubmitClick); //after filling out form, user can submit by pressing enter
+    $("calcCost").click(calcCostClick);
     showmap("map_canvas_car");
     showmap("map_canvas_taxi");
     showmap("map_canvas_transit");
-    
-    
-    
+    $('a[data-toggle="tab"]').click(function(event) {
+		event.preventDefault();
+		$(this).tab('show');
+	});
 }
 
 window.onload = init;
